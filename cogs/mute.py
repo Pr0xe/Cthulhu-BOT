6a0c3b08be2ec69a431b39553cde32a357f5733b
Pr0xe <csd4557@csd.uoc.gr>
2021-04-12 15:09:39 +0300

    mute & unmute in same file, and minor bugs fixed

--- cogs/mute.py
+++ cogs/mute.py
@@ -7,21 +7,43 @@ class Mute(commands.Cog):
         self.client = client
 
     @commands.command(pass_context = True)
-    @commands.has_permissions(manage_messages=True)
+    @commands.has_permissions(manage_roles=True)
     async def mute(self, ctx, member: discord.Member, *, reason=None):
         await member.edit(mute=True)
         embed=discord.Embed(title=" :mute: User Muted! :mute: ", description="**{0}** is mute by **{1}**!".format(member.mention, ctx.message.author.mention), color=0xff00f6)
         embed.add_field(name="Reason", value=reason, inline=False)
         await ctx.send(embed=embed)
-        em2u=discord.Embed(title=":mute: Server Muted :mute:", description="You are muted by **{0}**!".format(ctx.message.author.mention), color=0xff0000)
-        em2u.add_field(name="Reason", value=reason, inline=False)
-        await member.send(embed=em2u)
-    
+        print(f"user : {member} is voice muted by {ctx.author}")
+  
     @mute.error
     async def mute_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
-            embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
-            await ctx.say(embed=embed)
-
+            embed=discord.Embed(title="Permission Denied.", description="{0} You don't have permission to use this command".format(ctx.author.mention), color=0xff00f6)
+            await ctx.send(embed=embed)
+            print(f"Permission denied to {ctx.author}")
+        elif isinstance(error, commands.MissingRequiredArgument):
+            embed=discord.Embed(title="Arguments Missing", description="{0} Specify the user".format(ctx.author.mention), color=0xff00f6)
+            await ctx.send(embed=embed)
+            print(f"Not specified user by: {ctx.author}")
+            
+    @commands.command(pass_context = True)
+    @commands.has_permissions(manage_roles=True)
+    async def unmute(self, ctx, member: discord.Member, *, reason=None):
+        await member.edit(mute=False)
+        embed=discord.Embed(title=" :sound: User Unmuted! :sound:", description="**{0}** is now Unmuted!".format(member.mention), color=0xff00f6)
+        embed.add_field(name="Reason", value=reason, inline=False)
+        await ctx.send(embed=embed)
+        print(f"user : {member} is now unmuted by {ctx.author}")
+    
+    @unmute.error
+    async def unmute_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            embed=discord.Embed(title="Permission Denied.", description="{0} You don't have permission to use this command".format(ctx.author.mention), color=0xff00f6)
+            await ctx.send(embed=embed)
+            print(f"Permission denied to {ctx.author}")
+        elif isinstance(error, commands.MissingRequiredArgument):
+            embed=discord.Embed(title="Arguments Missing", description="{0} Specify the user".format(ctx.author.mention), color=0xff00f6)
+            await ctx.send(embed=embed)
+            print(f"Not specified user by: {ctx.author}")
 def setup(client):
     client.add_cog(Mute(client))
