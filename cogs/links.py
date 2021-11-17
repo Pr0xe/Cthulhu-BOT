8b3621da2e016c279acbf2d132d66ffc523587a5
Pr0xe <csd4557@csd.uoc.gr>
2021-11-17 21:50:21 +0200

    NEW: links connected with database

--- cogs/links.py
+++ cogs/links.py
@@ -14,13 +14,59 @@ class Links(commands.Cog):
         admin_channel = self.client.get_channel(762248895580733442)
         
         links = ['http', 'https']
+        message_con = message.content
         for text in links:
-            if ( text in message.content) and (message.channel == admin_channel):
-                embed=discord.Embed(title=":warning: Message Deleted :warning:", description=f"{message.author.mention} Links not allowed in this channel", color=0xff00f6)
+            if ( text in message_con) and (message.channel == admin_channel):
+                author_id = str(message.author)
+                author = await self.client.pg_con.fetch("SELECT * FROM deleted_messages WHERE author_id = $1",author_id)
+                if not author:
+                    await self.client.pg_con.execute("INSERT INTO deleted_messages (author_id, messages) VALUES($1, ARRAY[$2])", author_id, message_con)
+                    embed=discord.Embed(title=":warning: Message Deleted :warning:", description=f"{message.author.mention} Links not allowed in this channel", color=0xff00f6)
+                else:
+                    await self.client.pg_con.execute("UPDATE deleted_messages SET messages = array_append(messages, $1) WHERE author_id = $2",message_con,author_id)
+                    embed=discord.Embed(title=":warning: Message Deleted :warning:", description=f"{message.author.mention} Links not allowed in this channel", color=0xff00f6)
                 await admin_channel.purge(limit=1)
                 await admin_channel.send(embed=embed)
                 await log_channel.send(f"{message.author} posted a link, message deleted")
                 return
+    
+    @commands.command(pass_context = True)
+    @commands.has_permissions(ban_members=True)
+    async def messages(self, ctx):
+        deleted_embed = discord.Embed(
+                title=":recycle: Deleted messages :recycle:",
+                colour=0xFF0000)     
+        counter = await self.client.pg_con.fetch("SELECT COUNT(*) AS RowCnt FROM deleted_messages")
+        if counter==0:
+            clear_embed = discord.Embed(
+                title=":recycle: Deleted messages :recycle:",
+                colour=0xFF0000)
+            await ctx.send(embed=clear_embed)
+            return
+        else:
+            rows = await self.client.pg_con.fetch("SELECT messages FROM deleted_messages")
+            new_list=[]
+            new_list.append(rows)
+            deleted_embed.add_field(name="Deleted Message History", value=f"{new_list}", inline=False)
+            await ctx.send(embed=deleted_embed)
+            return
+    @commands.command(pass_context = True)
+    @commands.has_permissions(ban_members=True)
+    async def dmessages(self, ctx):
+        counter = await self.client.pg_con.fetch("SELECT COUNT(*) AS RowCnt FROM deleted_messages")
+        if counter==0:
+            clear_embed = discord.Embed(
+                title=":recycle: Deleted messages :recycle:",
+                colour=0xFF0000)
+            await ctx.send(embed=clear_embed)
+            return
+        else:
+            drep_embed = discord.Embed(
+                title=":recycle: Message history cleared :recycle:",
+                colour=0xFF0000)
+            await self.client.pg_con.execute("DELETE FROM deleted_messages")
+            await ctx.send(embed=drep_embed)
+
 
 def setup(client):
     client.add_cog(Links(client))
