c5f94f1f5720ccd6d7fbad93717c317269b7119a
Pr0xe <csd4557@csd.uoc.gr>
2022-06-16 17:02:00 +0300

    Whitespacing

--- bot/cogs/test.py
+++ bot/cogs/test.py
@@ -7,7 +7,7 @@ class Test(commands.Cog):
     
     @commands.command()
     async def test(self,ctx):
-        await ctx.reply(f'**Cthulhu is Online :green_circle: ** \n _Made by <@188771015751368704>_')
+        await ctx.reply(f'**Cthulhu is Online :green_circle: ** \n_Made by <@188771015751368704>_')
 
 def setup(bot):
     bot.add_cog(Test(bot))
