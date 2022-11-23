7fd532612a892df7f297197c3928ad05b1a10dac
Pr0xe <csd4557@csd.uoc.gr>
2022-11-23 13:19:20 +0200

    server_info: fixed typos for Dicord V2.0

--- bot/cogs/server_info.py
+++ bot/cogs/server_info.py
@@ -1,5 +1,4 @@
 import discord
-import json
 from discord.ext import commands
 from discord import Embed, Member
 
@@ -11,25 +10,24 @@ class ServerInfo(commands.Cog):
     async def server(self, ctx):
         log_channel = self.bot.get_channel(900492686581178398)
         embed = discord.Embed(
-        title="Server informations",
-        colour=0xFFA500,
-        timestamp=ctx.message.created_at)
-        embed.set_footer(text=f"Requested by {ctx.author}")
+            title="Server informations",
+            colour=discord.Color.random(),
+            timestamp=ctx.message.created_at
+        )
+        embed.set_footer(text=f"Requested by {ctx.author.display_name} ")
         guild = ctx.guild
-        owner = guild.owner_id
         statuses = [len(list(filter(lambda m: str(m.status) == "online",guild.members))),
                     len(list(filter(lambda m: str(m.status) == "idle",guild.members))),
                     len(list(filter(lambda m: str(m.status) == "dnd",guild.members))),
                     len(list(filter(lambda m: str(m.status) == "offline",guild.members)))]
 
         fields = [("Server Name", guild.name, True),
-                ("Server Owner", f'<@{owner}>', True),
-                ("Created at", ctx.guild.created_at.strftime("%d/%m/%Y"), True),
+                ("Server Owner", f'{guild.owner.mention}', True),
+                ("Created at", guild.created_at.strftime("%d/%m/%Y"), True),
                 ("Members", guild.member_count, True),
-                ("Banned members", len(await ctx.guild.bans()), True),
                 ("Humans", len(list(filter(lambda m: not m.bot,guild.members))),True),
                 ("Bots", len(list(filter(lambda m: m.bot,guild.members))), True), 
-                ("Roles", len(ctx.guild.roles), True),
+                ("Roles", len(ctx.message.guild.roles), True),
                 ("Statuses", f"🟢 {statuses[0]} 🟠 {statuses[1]} 🔴 {statuses[2]} ⚪ {statuses[3]}", False)]   
 
         for name, value, inline in fields:
